71dc9effd533741ad37795bdf0d84acee957e115
tingyuan <1932294867@qq.com>
2025-07-18 20:27:09 +0800

    -

--- eslint.config.js
+++ eslint.config.js
@@ -5,11 +5,15 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 import nodePlugin from 'eslint-plugin-n'
+import * as regexpPlugin from 'eslint-plugin-regexp'
+import sonarjs from 'eslint-plugin-sonarjs'
 
 export default tseslint.config([
   globalIgnores(['dist']),
+  regexpPlugin.configs['flat/recommended'],
+  sonarjs.configs.recommended,
   {
-    files: ['src/app/**/*.{ts,tsx}'],
+    files: ['src/app/**/*.{ts,tsx}', 'public/**/*.js'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
